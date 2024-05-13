09eb27a6382b2becf57326bfdd44d36c2bbbb522
Cbonchan <basterrasebastian@gmail.com>
2024-05-13 19:18:30 -0300

    mejoras a lo largo

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 from flask import Flask
-from db.characters import get_all_characters
 from flask_cors import CORS
+
+from time import sleep
+
+from db.characters import get_all_characters, get_character_by_id
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def home():
-    return "hola"
-    
 @app.route("/characters")
-def characters():
+def all_characters():
     return get_all_characters()
-   
+
+@app.route("/characters/<id>")
+def character_by_id(id):
+    sleep(1)
+    return get_character_by_id(id)
