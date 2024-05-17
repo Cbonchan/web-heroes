eee9030606e2d9f0c6901d9f215c9c6e9f445890
Cbonchan <basterrasebastian@gmail.com>
2024-05-17 13:46:13 -0300

    complete

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 from time import sleep
 
-from db.characters import get_all_characters, get_character_by_id, remove_character
+from db.characters import get_all_characters, get_character_by_id, remove_character, add_character
 
 app = Flask(__name__)
 CORS(app)
@@ -20,3 +20,27 @@ def character_by_id(id):
 @app.route("/characters/<id>",methods=["DELETE"])
 def remove_character_by_id(id):
     return {"success": remove_character(id)}
+
+@app.route("/characters", methods=["POST"])
+def create_character():
+    name= request.json.get("name")
+    names= request.json.get("names")
+    publisher= request.json.get("publisher")
+    gender= request.json.get("gender")
+    alingment= request.json.get("alingment")
+    image= request.json.get("image")
+    race= request.json.get("race")
+
+    id = int(get_all_characters()[-1].get("id"))+1
+
+    character= {
+        "name": name,
+        "names": names,
+        "publisher": publisher,
+        "gender": gender,
+        "alingment": alingment,
+        "image": image,
+        "race": race,
+        "id": id
+    }
+    return  { "success": add_character(character), "id" : id}
